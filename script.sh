7df4a112fb686201fc30452f58a565d79bb01999
Venu Vardhan Reddy Tekula <vt2182@nyu.edu>
2024-12-03 14:16:52 -0500

    Update script to handle dates properly

--- script.sh
+++ script.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-mkdir day-0$1
-cd day-0$1
+# Usage ./script.sh <day>
+# Example:
+# ./script.sh 01
+# ./script.sh 31
+
+mkdir day-$1
+cd day-$1
 touch sample.txt
 touch input.txt
 cp ../day.py .
 sed -i '' '1,2d' day.py
-mv day.py day-0$1.py
+mv day.py day-$1.py
 touch README.md
 cat <<EOL > README.md
-# Day-0$1
+# Day-$1
 
 > https://adventofcode.com/2024/day/$1
 
@@ -22,4 +27,4 @@ cat <<EOL > README.md
 
 ## Notes
 EOL
-echo "ready for day-0$1"
+echo "ready for day-$1"
